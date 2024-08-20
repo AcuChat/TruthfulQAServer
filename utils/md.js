f8e5e0b9869748781e8175d6fb23220b94533d2c
Michael Wood <michaelwood33311@icloud.com>
2024-08-20 09:04:08 -0400

    added function to identify list item that is solely a link

--- utils/md.js
+++ utils/md.js
@@ -1,7 +1,12 @@
 const markdownLinkRegex = /^\[([^\]]+)\]\(([^)]+)\)$/;
+const listItemLinkRegex = /^\s*[\*\+\-]\s+\[([^\]]+)\]\(([^)\s]+)(?:\s+"([^"]*)")?\)\s*$/;
 
 function isMarkdownLink(str) {
     return markdownLinkRegex.test(str);
+}
+
+function isListItemLink(markdownString) {
+    return listItemLinkRegex.test(markdownString);
 }
 
 exports.mdToAcuJson = async (md) => {
