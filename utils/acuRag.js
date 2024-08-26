dd5dbea4c1719768eb1df2abfcd7c15fe6e7af79
Michael Wood <michaelwood33311@icloud.com>
2024-08-26 10:22:42 -0400

    added handling previousLineIsHeading

--- utils/acuRag.js
+++ utils/acuRag.js
@@ -1,12 +1,26 @@
 const mdUtil = require('./md');
 
+
 exports.getLines = async (md) => {
     const rawLines = await mdUtil.mdToAcuJson(md);
     const lines = [];
+    let index = 0;
 
     for (let i = 0; i < rawLines.length; ++i) {
+        const line = rawLines[i];
+
         console.log('\n\n\n')
-        console.log(rawLines[i]);
+        console.log(line);
+
+        const {category} = line;
+    
+        switch (category) {
+            case 'previousLineIsHeading':
+                if (lines.length) lines[lines.length-1].category = 'heading';
+                break;
+                
+        }
+    
         break;
     }
 
